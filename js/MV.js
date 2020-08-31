100e59b94f6c3872b2f279367a301474b1f47157
marcovannu <marcovannucci99@gmail.com>
2020-08-31 08:37:07 +0200

    master

--- js/MV.js
+++ js/MV.js
@@ -1,9 +1,13 @@
 function openForm() {
-	  document.getElementById("myForm").style.display = "block";
+	document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-	  document.getElementById("myForm").style.display = "none";
+	document.getElementById("myForm").style.display = "none";
 }
 
-
+function AOS.init() {
+	easing: 'linear',
+	//duration: 1600,
+	once: true,
+};
